4577841cdb04983071b551ed232185adfc166659
FaraKhalili <farakhalili@gmail.com>
2024-07-23 21:51:23 -0700

    Test JUnit

--- features/login.feature
+++ features/login.feature
@@ -2,7 +2,7 @@ Feature: User Login
 
 	Scenario: Successful login
 		Given the user is on the nopCommerce login page
-		When the user enters valid credentials (username :"test@gmail.com" , password: "test@123")
+		When the user enters valid credentials (username :"test@test.com" , password: "test@123")
 		And the user clicks on the login button
 		Then the user should be redirected to the My Account page
 		And the user should see a welcome message
--- src/test/java/stepsDefinitions/StepDefinitions.java
+++ src/test/java/stepsDefinitions/StepDefinitions.java
@@ -1,38 +1,50 @@
 package stepsDefinitions;
 
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+	WebDriver driver;
 	@Given("the user is on the nopCommerce login page")
-	public void the_user_is_on_the_nop_commerce_login_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
+	public void navigateToLoginPage() {
+	   driver = new ChromeDriver();
+	    driver.get("https://demo.nopcommerce.com/");
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    driver.manage().window().maximize();	   
+	    driver.findElement(By.xpath("//a[normalize-space()='Log in']")).click();
+	    }
 
 	@When("the user enters valid credentials \\(username :{string} , password: {string})")
-	public void the_user_enters_valid_credentials_username_password(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	public void the_user_enters_valid_credentials_username_password(String user, String pwd) {
+
+		   driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(user);
+		   driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(pwd);
 	}
 
 	@When("the user clicks on the login button")
 	public void the_user_clicks_on_the_login_button() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
 	}
 
 	@Then("the user should be redirected to the My Account page")
 	public void the_user_should_be_redirected_to_the_my_account_page() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		boolean status =  driver.findElement(By.xpath("//a[@class='ico-account']")).isDisplayed();
+		Assert.assertEquals(status, true);
 	}
 
 	@Then("the user should see a welcome message")
 	public void the_user_should_see_a_welcome_message() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   boolean status = driver.findElement(By.xpath("//h2[normalize-space()='Welcome to our store']")).isDisplayed();
+	    Assert.assertEquals(status, true);
+	    driver.quit();
 	}
 
 
